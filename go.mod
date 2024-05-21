d5603f58d3b50651bb19a0a3d74e6d1268fe688c
Meadow <meadowingc@gmail.com>
2024-05-21 16:58:00 -0600

    initial

---
+++ go.mod
@@ -0,0 +1,4 @@
+module codeberg.org/meadowingc/guestbook
+
+go 1.22
+
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "log"
+
+func main() {
+	// viper.SetConfigName("config")
+	// viper.SetConfigType("yaml")
+	// viper.AddConfigPath(".")
+
+	// err := viper.ReadInConfig()
+	// if err != nil {
+	// 	log.Fatalf("error reading config: %s", err)
+	// }
+
+	log.Println("Hi!")
+
+}
