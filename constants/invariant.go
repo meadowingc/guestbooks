ff35f0d090df242978254fc25af4e0edb9c4c6f3
Meadow <meadowingc@gmail.com>
2024-08-20 08:42:53 -0600

    update domain to cafe

--- constants/invariant.go
+++ constants/invariant.go
@@ -2,7 +2,7 @@ package constants
 
 const (
 	// public URL
-	PUBLIC_URL           = "https://guestbooks.meadowing.club"
+	PUBLIC_URL           = "https://guestbooks.meadow.cafe"
 	MAX_MESSAGES_TO_SHOW = 500
 	MAX_MESSAGE_LENGTH   = 2500
 )
